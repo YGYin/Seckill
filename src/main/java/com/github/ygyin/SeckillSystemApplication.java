910cd7a4a1498c918ce3f72e140247fb977e38d3
YGYin <ygyin0307@gmail.com>
2023-03-18 11:49:16 +0800

    First Commit

---
+++ src/main/java/com/github/ygyin/SeckillSystemApplication.java
@@ -0,0 +1,13 @@
+package com.github.ygyin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeckillSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeckillSystemApplication.class, args);
+    }
+
+}
